3a2df6b927ece191ae4f496b5acbb7c989ad043c
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-06-19 22:01:51 +0100

    Fichero html algo dinamico con csv y reader

--- mi_cartera/__init__.py
+++ mi_cartera/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    f = open("movements.dat", "r")
+    reader = csv.reader(f, delimiter=",", quotechar='"')
+    movements = list(reader)
+
+    return render_template("index.html", the_movements=movements) #Con esto le estoy diciendo que en el render utilice los movements
+
+#Esto mezcla el index.htm con los datos movements. Donde jinja2 encuentre una doble llave {{}}, le pone los datos.
