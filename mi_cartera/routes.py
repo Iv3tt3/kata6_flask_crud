55bf48ced854c3d1eac972232a185dbc1427bde4
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-06-20 21:57:56 +0100

    La peticion POST ya funciona

--- mi_cartera/routes.py
+++ mi_cartera/routes.py
@@ -1,5 +1,5 @@
 from mi_cartera import app #tambien podriamos hacer from . import app
-from flask import render_template
+from flask import render_template, request, redirect
 import csv
 
 
@@ -11,8 +11,17 @@ def index():
 
     return render_template("index.html", the_movements=movements) 
 
-@app.route("/new_movement")
+@app.route("/new_movement", methods=["GET", "POST"]) #Con esta etiqueta acepta peticiones GET i POST
+#Con la variable request.method puedes ver si es GET o es POST
 def new_mov():
-    return render_template("new_mov_form.html")
+    if request.method == 'GET':
+        return render_template("new_mov_form.html")
+    else:
+        data = request.form
+        f = open("movements.dat", "a") #a de append, anade no lee como el r de reader
+        writer = csv.DictWriter(f, fieldnames=data.keys())
+        writer.writerow(data)
 
+        return redirect("/") #Es otra funcion de flask que te redirige a una ruta, y le ponemos la ruta de inicio
 
+#request es una instancia que ya existe y el metodo request.form es el que recupera la informacion
