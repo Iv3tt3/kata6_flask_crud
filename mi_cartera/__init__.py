fb0fa4219adc142a8630fe90bfbfdac41a778283
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-06-20 16:15:15 +0100

    Ahora usando DictReader

--- mi_cartera/__init__.py
+++ mi_cartera/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     f = open("movements.dat", "r")
-    reader = csv.reader(f, delimiter=",", quotechar='"')
+    reader = csv.DictReader(f)
     movements = list(reader)
 
     return render_template("index.html", the_movements=movements) #Con esto le estoy diciendo que en el render utilice los movements
